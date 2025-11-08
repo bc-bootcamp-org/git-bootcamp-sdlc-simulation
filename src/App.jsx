88e13f70b17f498de5c42ffd3b5b47dff23e73cc
puzancozzu <puzancozzu@gmail.com>
2025-11-08 21:21:21 +0545

    feat: changes the group name - Team D to Team Mukesh G.

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ function App() {
       ]
     },
     { 
-      name: 'Team D', 
+      name: 'Team Mukesh G.', 
       members: [
         <TeamMember1D key="teamMember1D" />,
         <TeamMember2D key="teamMember2D" />,
