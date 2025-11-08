a34f8867a425cce4851c607700c682ac9fee16e6
BakaYuki <mukeshpr443@gmail.com>
2025-11-08 21:27:59 +0545

    fix: Team name changed

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ function App() {
       ]
     },
     { 
-      name: 'Team D', 
+      name: 'Poojan Don', 
       members: [
         <TeamMember1D key="teamMember1D" />,
         <TeamMember2D key="teamMember2D" />,
