ed573e7cbba899fb2c3393d45904b5dcc4e249b2
bikrant0 <shilpakarbikrant@gmail.com>
2025-11-09 14:36:26 +0545

    feat: last name added

--- src/team-c/teamMember24.jsx
+++ src/team-c/teamMember24.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TeamMember4 = () => {
-  const name = "Bikrant"; // Leave blank for now
+  const name = "Bikrant Shilpakar"; // Leave blank for now
   
   // Return null if name is blank
   if (!name.trim()) {
